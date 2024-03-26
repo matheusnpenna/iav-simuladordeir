d0e1a26e50576848f08381150d0b4ab19cd11ed5
Matheus Penna <matheus.penna@blitzpay.com.br>
2024-03-26 15:19:48 -0300

    update color

--- src/views/TutorialView.vue
+++ src/views/TutorialView.vue
@@ -2,7 +2,7 @@
   <main class="py-5">
     <h2 class="text-center text-4xl mb-16">Como Doar</h2>
 
-    <p>
+    <p class="text-slate-500">
       1. Após terminar de inserir seus dados da declaração de imposto de renda, antes de finalizar o
       processo:<br /><br />
 
@@ -12,14 +12,14 @@
 
       Municipal (x)<br /><br />
 
-      <span class="font-bold">
+      <span class="text-black font-bold">
         FMDCA - Fundo Municipal dos direitos da Criança e do Adolescente
       </span><br /><br />
 
       FIA - ACAUÃ - PI<br />
       CNPJ: 33.853.909/0001-41<br /><br />
 
-      <span class="font-bold">
+      <span class="text-black font-bold">
         FMDPI - Fundo Municipal de Direito da Pessoa Idosa
       </span><br /><br />
 
@@ -27,14 +27,16 @@
       CNPJ: 48.992.364/0001-21<br /><br />
     </p>
 
-    <p>
+    <p class="text-slate-500">
       4. Acesse: <br />
-      <span class="font-bold">Declaração > Imprimir > Imprimir DARF - Doações diretas na declaração - ECA</span><br />
-      <span class="font-bold">Declaração > Imprimir > Imprimir DARF - Doações diretas na declaração - Pessoa
+      <span class="text-black font-bold">Declaração > Imprimir > Imprimir DARF - Doações diretas na declaração -
+        ECA</span><br />
+      <span class="text-black font-bold">Declaração > Imprimir > Imprimir DARF - Doações diretas na declaração - Pessoa
         Idosa</span><br /><br />
 
-      5. <span class="font-bold">Envie seu comprovante de pagamento ao IAV</span> pelo e-mail <span
-        class="font-bold">captacao@institutoaguavivia.org.br</span> para que seja computado junto aos projetos do FIA.
+      5. <span class="text-black font-bold">Envie seu comprovante de pagamento ao IAV</span> pelo e-mail <span
+        class="text-black font-bold">captacao@institutoaguavivia.org.br</span> para que seja computado junto aos
+      projetos do FIA.
     </p>
 
 
